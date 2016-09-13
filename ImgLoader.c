f990dbeda20ab467d2fac0632f2c580d91c0e8f2
Frankie Berry <fmb34@nau.edu>
2016-09-12 21:52:46 -0700

    Initialized the header of the ImgLoader file, including name declaration and pre-processor directives

---
+++ ImgLoader.c
@@ -0,0 +1,14 @@
+//
+//  FBerry_CS430_ImageLoader.c
+//  CS430 Project 1
+//
+//  Frankie Berry
+//
+
+
+// pre-processor directives
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{}

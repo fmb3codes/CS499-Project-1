f15fa2cb69ea6227ec26b22cd31b1160d213f927
Frankie Berry <fmb34@nau.edu>
2016-09-15 05:59:48 +0100

    Implemented read_header_data function through use of manually reading the first few lines and storing them in the buffer accordingly. Also implemented error checking at various stages for invalid values as well as checking to see if a comment exists or not

--- ImgLoader.c
+++ ImgLoader.c
@@ -53,7 +53,7 @@ int main(int argc, char** argv)
 	if(fp == NULL)
 	{
 		fprintf(stderr, "Error: File didn't open properly; filename may be incorrect or file may not exist.\n");
-		return -1;
+		exit(1);
 	}
 	
 	printf("File opened successfully\n");
@@ -89,11 +89,138 @@ int main(int argc, char** argv)
 	
 	read_header_data(filetype, inputname);
 	
+	return 0;
+	
 }
 
 void read_header_data(char* file_format, char* input_file_name)
 {
+	FILE *fp;
 	
+	fp = fopen(input_file_name, "r");
+	
+	printf("Input file name is: %s\n", input_file_name);
+	
+	if(fp == NULL)
+	{
+		fprintf(stderr, "Error: File didn't open properly; filename may be incorrect or file may not exist.\n");
+		exit(1); // exits out of program due to error
+	}
+	
+	char* current_line;
+	
+	printf("Getting to this point and file_format is: %s okay\n", file_format);
+
+	//fgets(current_line, 1024, fp);
+	
+	printf("Current line is %s and length is %d\n", current_line, strlen(current_line));
+	
+	printf("Passed fgets\n");
+	//printf("About to read file format into buffer:%s\n", current_line);
+	
+	printf("Current stored buffer file format is: %s\n", buffer->file_format);
+	
+	
+	strcpy(buffer->file_format, fgets(current_line, 1024, fp));
+	
+	//(*buffer).file_format = (unsigned char *)malloc(3);
+	
+	printf("Current stored buffer file format is: %s\n", buffer->file_format);
+	
+	//buffer->file_format = fgets(current_line, 1024, fp); // stores file format 
+	
+
+	//printf("File format read in is: %s\n", buffer->file_format);
+	
+	if(strcmp(buffer->file_format, "P3\n") != 0 && strcmp(buffer->file_format, "P6\n") != 0)
+	{
+		fprintf(stderr, "Error: Given file format is neither P3 nor P6.\n");
+		exit(1); // exits out of program due to error		
+	}
+	
+	
+	 fgets(current_line, 1024, fp);
+	 
+	 printf("Current_line contains: %s\n", current_line);
+	 
+	 int is_comment = 1;
+	 
+	 //checks for comment or empty line
+	 if(*current_line == '#')
+	 {
+		strcpy(buffer->file_format, current_line);
+		is_comment = 0;
+		
+		fgets(current_line, 1024, fp);
+		 
+		char* width_and_height = strtok(current_line, " ");
+		
+		int width = atoi(width_and_height);
+		strcpy(buffer->file_width, width_and_height);
+		printf("Current width is: %d\n",width);
+		
+		width_and_height = strtok(NULL, " ");
+		
+		int height = atoi(width_and_height);
+		strcpy(buffer->file_height, width_and_height);
+		printf("Current height is: %d\n", height);
+		
+		if(height < 0 || width < 0)
+		{
+			fprintf(stderr, "Error: Invalid height or width.\n");
+		    exit(1); // exits out of program due to error
+		}
+		
+		strcpy(buffer->file_maxcolor, fgets(current_line, 1024, fp));
+		
+		if(atoi(current_line) < 0 || atoi(current_line) > 255)
+		{
+			fprintf(stderr, "Error: Max color value is off\n");
+		    exit(1); // exits out of program due to error	
+		}
+		
+		printf("Current max color value is: %d\n", atoi(current_line));
+		
+		
+	 }
+	 
+	 //evaluates if there was no comment, simply changing the flow of reading in each line (same process as above minus the comment read/assignment"
+	 if(is_comment == 1)
+	 {
+	    char* width_and_height = strtok(current_line, " ");
+		
+		int width = atoi(width_and_height);
+		strcpy(buffer->file_width, width_and_height);
+		printf("Current width is: %d\n",width);
+		
+		width_and_height = strtok(NULL, " ");
+		
+		int height = atoi(width_and_height);
+		strcpy(buffer->file_height, width_and_height);
+		printf("Current height is: %d\n", height);
+		
+		if(height < 0 || width < 0)
+		{
+			fprintf(stderr, "Error: Invalid height or width.\n");
+		    exit(1); // exits out of program due to error
+		}
+		
+		strcpy(buffer->file_maxcolor, fgets(current_line, 1024, fp));
+		
+		if(atoi(current_line) < 0 || atoi(current_line) > 255)
+		{
+			fprintf(stderr, "Error: Max color value is off\n");
+		    exit(1); // exits out of program due to error	
+		}
+		
+		printf("Current max color value is: %d\n", atoi(current_line));
+		
+	 }
+	 
+	
+	
+	
+	fclose(fp);
 }
 
 void read_image_data(char* file_format, char* input_file_name)
